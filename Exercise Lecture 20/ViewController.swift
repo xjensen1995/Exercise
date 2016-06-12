7136b836def3c6b59b9bc6665ba80d0c3d935b40
Timm Jensen <timmjensen@timms-macbook-pro.local>
2016-06-12 12:13:46 +0200

    Initial Commit

---
+++ Exercise Lecture 20/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise Lecture 20
+//
+//  Created by Timm Jensen on 12/06/2016.
+//  Copyright © 2016 ZeroApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
