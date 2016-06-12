a721fc4b00a90e0cedf8ce4d711bd4fe4e8a086b
Timm Jensen <timmjensen@Timms-MacBook-Pro.local>
2016-06-12 12:19:13 +0200

    Comment added

--- Exercise Lecture 20/ViewController.swift
+++ Exercise Lecture 20/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    // Comment has been made!
 
 }
 
